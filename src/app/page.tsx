906b6f5c9735a765f64f25a07ffeb5f86ebacac2
cyberjj999 <lyejiajun99@gmail.com>
2025-02-14 16:18:31 +0800

    feat: add shadcn card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-	return <main>home</main>;
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface Recipe {
+	title: string;
+	image: string;
+	time: number;
+	description: string;
+	vegan: boolean;
+	id: string;
+}
+
+// Function to fetch data (using Mock JSON Server)
+async function getRecipes(): Promise<Recipe[]> {
+	const result = await fetch("http://localhost:4000/recipes");
+
+	return result.json();
+}
+
+export default async function Home() {
+  // invoke the getRecipes to get the data.
+	const recipes = await getRecipes();
+
+	return (
+		<main>
+      {/* bunch of styling stuff to loop throuhg each recipe using ShadCn */}
+			<div className="grid grid-cols-3 gap-8">
+				{recipes.map((recipe) => (
+					<Card key={recipe.id} className="flex flex-col justify-between">
+						<CardHeader className="flex-row gap-4 items-center">
+							<div>
+								<CardTitle>{recipe.title}</CardTitle>
+								<CardDescription>{recipe.time} mins to cook.</CardDescription>
+							</div>
+						</CardHeader>
+						<CardContent>
+							<p>{recipe.description}</p>
+						</CardContent>
+						<CardFooter className="flex justify-between">
+							<button>View Recipe</button>
+              {/* output vegan if its true */}
+							{recipe.vegan && <p>Vegan!</p>}
+						</CardFooter>
+					</Card>
+				))}
+			</div>
+		</main>
+	);
 }
